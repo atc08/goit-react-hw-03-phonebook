54b05e3a766acb6b08de22e6e7fee1ad397ef053
Yevhenii Korkots <atc0884@gmail.com>
2020-12-12 00:29:30 +0200

    delete comments

--- src/App.js
+++ src/App.js
@@ -2,16 +2,10 @@ import { Component } from 'react';
 import ContactForm from './components/ContactForm';
 import ContactList from './components/ContactList';
 import Filter from './components/Filter';
-import { v4 as uuidv4 } from 'uuid';
 
 class App extends Component {
   state = {
-    contacts: [
-      // { id: uuidv4(), name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: uuidv4(), name: 'Hermione Kline', number: '443-89-12' },
-      // { id: uuidv4(), name: 'Eden Clements', number: '645-17-79' },
-      // { id: uuidv4(), name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
